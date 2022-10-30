cc42de532f7757855a2240cd462bd8b18e689746
Ergün İZMİRLİOĞLU <ergun.izmirlioglu@std.yildiz.edu.tr>
2022-10-30 04:54:34 +0300

    added lazy loading page feature

--- components/Video/VideosList.js
+++ components/Video/VideosList.js
@@ -1,16 +1,56 @@
 import styles from "./VideosList.module.css";
 
+import { useEffect, useRef, useState } from "react";
+import useOnScreen from "../../hooks/useOnScreen";
 import Card from "../UI/Card";
 import VideoItem from "./VideoItem";
+import { LoadingSpinner } from "../../styles/Icons";
 
 const VideosList = (props) => {
+  const screenRef = useRef();
+  const screenRefValue = useOnScreen(screenRef);
+  const [isScreenRef, setIsScreenRef] = useState(false);
+
+  const [selectedData, setSelectedData] = useState(props.videos.slice(0, 16));
+
+  useEffect(() => {
+    if (!isScreenRef) setIsScreenRef(screenRefValue);
+  }, [screenRefValue]);
+
+  const loadMore = () => {
+    setIsScreenRef(false);
+    const currlen = selectedData.length;
+
+    if (currlen >= props.videos.length) {
+      screenRef.current.remove();
+      return;
+    }
+
+    if (!props.videos[currlen + 16]) {
+      setSelectedData([
+        ...selectedData,
+        ...props.videos.slice(currlen, props.videos.length),
+      ]);
+    } else {
+      setSelectedData([
+        ...selectedData,
+        ...props.videos.slice(currlen, 16 + selectedData.length),
+      ]);
+    }
+  };
+
   return (
     <Card className={styles.list}>
       <ul>
-        {props.videos.map((p) => (
+        {selectedData.map((p) => (
           <VideoItem key={p.id} id={p.id} img={p.images[0]} title={p.title} />
         ))}
       </ul>
+      <div ref={screenRef} className="centered">
+        <LoadingSpinner />
+      </div>
+
+      {isScreenRef && loadMore()}
     </Card>
   );
 };
--- styles/Icons.js
+++ styles/Icons.js
@@ -1,3 +1,5 @@
+import style from "./Icons.module.css";
+
 export const Bookmark = () => (
   <svg
     version="1.1"
@@ -61,5 +63,11 @@ export const Toggle = () => (
     <rect width="100" height="20"></rect>
     <rect y="30" width="100" height="20"></rect>
     <rect y="60" width="100" height="20"></rect>
+  </svg>
+);
+
+export const LoadingSpinner = () => (
+  <svg className={style.spinner} viewBox="0 0 24 24" width="30" height="30">
+    <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" />
   </svg>
 );
