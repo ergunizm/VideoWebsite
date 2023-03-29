afdd28a269c8c866a6579fb2419b48e8fb5f6616
ergunizm <21011604@std.yildiz.edu.tr>
2023-03-29 16:09:03 +0300

    updated head

--- pages/404.js
+++ pages/404.js
@@ -1,18 +1,30 @@
+import Head from "next/head";
 import Link from "next/link";
+import { Fragment } from "react";
 import styles from "./404.module.css";
 
 const NotFoundPage = () => {
   return (
-    <div className={styles.container}>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, there is nothing to see here</p>
-      <p>Use the link below to go homepage</p>
-      <div className={styles.linkDiv}>
-        <Link className={styles.link} href="/">
-          Homepage
-        </Link>
+    <Fragment>
+      <Head>
+        <title>Morningstar</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <link rel="icon" href="/logo.ico" />
+      </Head>
+      <div className={styles.container}>
+        <h1>404 - Page Not Found</h1>
+        <p>Sorry, there is nothing to see here</p>
+        <p>Use the link below to go homepage</p>
+        <div className={styles.linkDiv}>
+          <Link className={styles.link} href="/">
+            Homepage
+          </Link>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
